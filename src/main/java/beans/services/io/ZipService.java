4577e8b49e9e6d9f7d50e17832c2db4ac48aeb10
Sergei Balashov <timid@bk.ru>
2015-12-21 00:21:07 +0300

    fix xmlService

--- src/main/java/beans/services/io/ZipService.java
+++ src/main/java/beans/services/io/ZipService.java
@@ -1,8 +1,6 @@
 package beans.services.io;
 
-import com.google.common.io.CharStreams;
 import org.springframework.stereotype.Service;
-import settings.Settings;
 
 import java.io.*;
 import java.util.zip.ZipEntry;
@@ -14,7 +12,7 @@ import java.util.zip.ZipInputStream;
 @Service
 public class ZipService {
     public InputStream getInputStream(File zip, String entry) {
-        ZipInputStream zin = null;
+        ZipInputStream zin;
         try {
             zin = new ZipInputStream(new FileInputStream(zip));
             for (ZipEntry e; (e = zin.getNextEntry()) != null; ) {
@@ -35,25 +33,5 @@ public class ZipService {
             return false;
         }
         return true;
-    }
-
-    public File unzipEntry(File zip, String entry) {
-        if (zip == null || entry == null || !zip.isFile()) {
-            return null;
-        }
-        File unzip = null;
-        try {
-            InputStream inputStream = getInputStream(zip, entry);
-            unzip = new File(Settings.TMP);
-            unzip.createNewFile();
-            FileWriter fileWriter = new FileWriter(unzip);
-            String content = CharStreams.toString(new InputStreamReader(inputStream, "windows-1251"));
-            fileWriter.write(content);
-            fileWriter.close();
-            inputStream.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return unzip;
     }
 }
--- src/main/java/beans/services/xml/XmlService.java
+++ src/main/java/beans/services/xml/XmlService.java
@@ -1,40 +1,35 @@
 package beans.services.xml;
 
+import com.google.common.io.CharStreams;
+import org.jdom.Document;
+import org.jdom.JDOMException;
+import org.jdom.input.SAXBuilder;
 import org.springframework.stereotype.Service;
-import org.w3c.dom.Document;
-import org.xml.sax.SAXException;
 
-import javax.xml.XMLConstants;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringReader;
 
 /**
  * Created by Timid on 20.12.2015.
  */
 @Service
 public class XmlService {
-    private final String LANGUAGE = XMLConstants.W3C_XML_SCHEMA_NS_URI;
-    private final DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 
     public Document getXmlDocument(InputStream inputStream) {
         if (inputStream == null) {
             return null;
         }
-        Document document = null;
-        DocumentBuilder dBuilder = null;
         try {
-            dBuilder = factory.newDocumentBuilder();
-            document = dBuilder.parse(inputStream);
-        } catch (ParserConfigurationException e) {
-            e.printStackTrace();
-        } catch (SAXException e) {
-            e.printStackTrace();
+            String content = CharStreams.toString(new InputStreamReader(inputStream, "windows-1251"));
+            SAXBuilder saxBuilder = new SAXBuilder();
+            return saxBuilder.build(new StringReader(content));
         } catch (IOException e) {
             e.printStackTrace();
+        } catch (JDOMException e) {
+            e.printStackTrace();
         }
-        return document;
+        return null;
     }
 }
